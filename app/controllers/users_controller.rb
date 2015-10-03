56df57eb023a4dcbf04318158eb082037a056470
Alina Jahnes <alina.jahnes@gmail.com>
2015-10-02 22:40:32 -0400

    add user controller and post controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render layout: false
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :root
+    else
+      flash.now[:error] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
