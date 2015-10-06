5a98adc4e84d7403327b09a3228c966dd6dcccd6
Alina Jahnes <alina.jahnes@gmail.com>
2015-10-05 23:25:53 -0400

    add paperclip gem and allow user upload picture as avatar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004003457) do
+ActiveRecord::Schema.define(version: 20151005190612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,14 @@ ActiveRecord::Schema.define(version: 20151004003457) do
   add_index "posts", ["slug"], name: "index_posts_on_slug", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "username",            null: false
+    t.string   "password_digest",     null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
