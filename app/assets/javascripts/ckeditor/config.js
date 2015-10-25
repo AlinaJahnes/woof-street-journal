68e7b4c83411c5809bd7989563e25a43d9204933
Alina Jahnes <alina.jahnes@gmail.com>
2015-10-25 16:23:03 -0400

    add youtube plugin for ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,13 +1,16 @@
 CKEDITOR.editorConfig = function (config) {
+  config.extraPlugins = 'youtube';
 
   config.toolbar_mini = [
-    ["Bold",  "Italic",  "Underline", "Subscript",  "Superscript"], 
-    ["Image", "Smiley", "Link", "Unlink"],
+    ["Source"],
+    ["Bold", "Italic", "Subscript", "Superscript"], 
+    ["Image", "Youtube","Smiley", "Link", "Unlink"],
     ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
   ];
   config.toolbar = "mini";
   config.filebrowserImageBrowseUrl = '/ckeditor/pictures';
   config.filebrowserImageUploadUrl = '/ckeditor/pictures';
   config.language = 'en';
- config.uiColor = '#BCD8E6';
+  config.uiColor = '#BCD8E6';
+
 };
