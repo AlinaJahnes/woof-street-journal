a4bf34f1529ef20629052ce7ee54eccdc366661e
Alina Jahnes <alina.jahnes@gmail.com>
2015-10-27 15:16:22 -0400

    add method to post model to get the first image of every post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates :title, :body, :category, presence: true  
+
+  def first_image
+    /img\s+[^>]*src\s*=\s*"(.+?)"/.match(self.body)
+  end
+
 end
