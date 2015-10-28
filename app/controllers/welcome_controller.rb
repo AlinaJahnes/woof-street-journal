af8bd227a2d1985fa19501b7fbd5e873ff0ff9b3
Alina Jahnes <alina.jahnes@gmail.com>
2015-10-27 23:39:08 -0400

    add logic to index method to show post collection

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index    
     @user = User.new
+    all_posts = Post.order('created_at')
+    @first_post = all_posts.last
+    @posts = all_posts.take(all_posts.count - 1)
   end
 end
